95b4f9a56e781cdcb1d668509227d786a1f51eda
Emanuel Fuentes <fuentes.emanuel18@gmail.com>
2023-10-20 11:48:28 -0300

    ADDED: Configuraciones generales

---
+++ app/config/configuration.php
@@ -0,0 +1,7 @@
+<?php
+
+    //Mercado Pago credentials
+    define('MP_ACCESS_TOKEN', 'APP_USR-8709825494258279-092911-227a84b3ec8d8b30fff364888abeb67a-1160706432');
+
+    //Mercado Pago integrator id
+    define('MP_INTEGRATOR_ID', 'dev_24c65fb163bf11ea96500242ac130004');
