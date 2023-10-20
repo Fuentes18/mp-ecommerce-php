6fc19a1971eda76cfd4e521a3732471e365033a8
Emanuel Fuentes <fuentes.emanuel18@gmail.com>
2023-10-20 12:45:02 -0300

    EDITED: Ruta base

--- app/config/configuration.php
+++ app/config/configuration.php
@@ -1,4 +1,6 @@
 <?php
+    // Base URL
+    define("RUTE_URL", (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] === "on" ? "https" : "http") . "://$_SERVER[HTTP_HOST]");
 
     //Mercado Pago credentials
     define('MP_ACCESS_TOKEN', 'APP_USR-8709825494258279-092911-227a84b3ec8d8b30fff364888abeb67a-1160706432');
